0febd29ea4389f37017ef74f9bf877fda0413459
Shrijana Maharjan <shrezana2060@gmail.com>
2023-09-27 11:57:31 -0700

    inital commit

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<div class="footer-section footer-section1">
+<div class="footer-brand">
+    <a href="index.php">
+        <h1 class="footer-heading">MoveiHeal</h1>
+    </a> 
+</div>    
+</div>
+
+<div class="footer-section  footer-section3">
+    <p>© 2023 MovieHeal. Created by Charm. </p>
+    <a>Advertising</a>
+    <a>Privacy Policy</a>
+    <a href="contact-us.php">Contact</a>
+</div>
